1e13634d8ac709274dbb037982b1ce8bb50fe210
Aniket Dhande <dhandeaniket52@gmail.com>
2026-03-30 02:44:47 +0530

    changed version

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const menuroutes = require('./routes/menuroutes');
 app.use('/menu',menuroutes);
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000')
 })
  
